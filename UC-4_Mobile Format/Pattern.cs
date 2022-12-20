ad899157be3c9abad550b0bebe790dc263422a9a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:26:01 +0530

    Check validation for mobile format

---
+++ UC-4_Mobile Format/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_4_Mobile_Format
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = "^+[1-9]{2}[: :][0-9]{10}$";
+        public bool Validate_PhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, REGEX_CODE);
+        }
+    }
+}
---
+++ UC-4_Mobile Format/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_4_Mobile_Format
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter the Phone Number: ");
+            string phonenumber = Console.ReadLine();
+            bool num = pat.Validate_PhoneNumber(phonenumber);
+            Console.WriteLine(num);
+            if (num)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
