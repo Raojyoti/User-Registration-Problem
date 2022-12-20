3b4fa9bb0a72ed6cc7c8f63a7ce588ab9c9c3016
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:21:43 +0530

    Check validation for email id

---
+++ UC-3_Valid_Email/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_3_Valid_Email
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = @"^([abc]+)(\.[a-z0-9_\+\-]+)?@([bl]+)\.([co]{2,4})(\.[a-z]{2})?$";
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, REGEX_CODE);
+        }
+    }
+}
---
+++ UC-3_Valid_Email/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_3_Valid_Email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter the Email Id: ");
+            string email = Console.ReadLine();
+            bool id = pat.Validate_Email(email);
+            Console.WriteLine(id);
+            if (id)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
