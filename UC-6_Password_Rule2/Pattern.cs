de2341df31ca9ee82a50013025b77ab619c83614
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:32:42 +0530

    Check password validation for rule-2

---
+++ UC-6_Password_Rule2/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_6_Password_Rule2
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = "^[a-zA-Z]{8}$";
+        public bool Validate_Password(string password)
+        {
+            return Regex.IsMatch(password, REGEX_CODE);
+        }
+    }
+}
---
+++ UC-6_Password_Rule2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_6_Password_Rule2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            bool num = pat.Validate_Password(password);
+            Console.WriteLine(num);
+            if (num)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
