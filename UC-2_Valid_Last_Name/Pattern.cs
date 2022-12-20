f817066812716c7c59f79f240ce9d0ef6ebecd4d
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:18:50 +0530

    Check validation for last name

---
+++ UC-2_Valid_Last_Name/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_2_Valid_Last_Name
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = "^[A-Z]{1}[a-z]{2}$";
+        public bool Validate_LastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, REGEX_CODE);
+        }
+    }
+}
---
+++ UC-2_Valid_Last_Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_2_Valid_Last_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter the Last Name: ");
+            string lastname = Console.ReadLine();
+            bool name = pat.Validate_LastName(lastname);
+            Console.WriteLine(name);
+            if (name)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
