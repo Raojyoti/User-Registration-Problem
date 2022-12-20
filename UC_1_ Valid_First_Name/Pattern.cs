db47e524ef97fd18936c78eb13b5f17a8744b7cf
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-20 19:16:10 +0530

    Check validation for first name

---
+++ UC_1_ Valid_First_Name/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UC_1__Valid_First_Name
+{
+    public class Pattern
+    {
+        public static string REGEX_CODE = "^[A-Z]{1}[a-z]{2}$";
+        public bool Validate_FirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, REGEX_CODE);
+        }
+    }
+}
---
+++ UC_1_ Valid_First_Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_1__Valid_First_Name
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter the First Name: ");
+            string firstname = Console.ReadLine();
+            bool name = pat.Validate_FirstName(firstname);
+            Console.WriteLine(name);
+            if (name)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
